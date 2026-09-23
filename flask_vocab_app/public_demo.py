1a91ca7f87d4f307a07b1927847141346ee5a764
tombryson <thomas.bryson@outlook.com>
2026-09-24 00:13:21 +1000

    Replace curriculum dropdowns with dedicated reading pages

--- flask_vocab_app/public_demo.py
+++ flask_vocab_app/public_demo.py
@@ -18,7 +18,7 @@ READ_ENDPOINTS = frozenset({
     'public_demo_account',
     'word_post.home', 'word_post.legacy_home', 'word_post.assets', 'word_post.licenses', 'static',
     'ui_preferences.appearance',
-    'curriculum.index', 'curriculum.unit_page', 'learning.read_session',
+    'curriculum.index', 'curriculum.outcomes_page', 'curriculum.topic_page', 'curriculum.unit_page', 'learning.read_session',
     'vocab.vocab_list', 'vocab.inventory', 'learning.state', 'learning.asset',
     'live_conversation.scenarios', 'live_conversation.options', 'user_sessions.read',
     'step_conversation.options', 'step_conversation.history',
